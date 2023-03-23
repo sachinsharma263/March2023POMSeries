f3ea7646edb09d1bc4af72775d47ad4db8725875
sachinsharma263 <sachinsharma263@gmail.com>
2023-03-23 16:45:29 +0530

    regpage added

---
+++ src/main/java/com/qa/trcrm/pages/DealsPage.java
@@ -0,0 +1,15 @@
+package com.qa.trcrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.trcrm.base.BasePage;
+
+public class DealsPage extends BasePage {
+
+	WebDriver driver;
+	
+	public DealsPage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+}
