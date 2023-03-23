faeb3fd44b118c60c222a0e351a16c93e480824f
sachinsharma263 <sachinsharma263@gmail.com>
2023-03-23 23:20:46 +0530

    profilepage updated

--- src/main/java/com/qa/trcrm/pages/ProfilePage.java
+++ src/main/java/com/qa/trcrm/pages/ProfilePage.java
@@ -11,8 +11,13 @@ public class ProfilePage extends BasePage {
 	public ProfilePage(WebDriver driver) {
 		this.driver = driver;
 	}
-	public void m1()
-	{
-	System.out.println("m1");
+
+	public void m1() {
+		System.out.println("m1");
+	}
+
+	public void m2() {
+		System.out.println("m2");
+
 	}
 }
