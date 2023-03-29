973a5a443aa3d91952ca6c7540dc6b1b60fe2ad1
sachinsharma263 <sachinsharma263@gmail.com>
2023-03-29 23:46:40 +0530

    log

--- src/main/java/com/qa/trcrm/utils/Log.java
+++ src/main/java/com/qa/trcrm/utils/Log.java
@@ -2,7 +2,7 @@ package com.qa.trcrm.utils;
 
 /**
  * This post will explain the integration of any java logger (let’s say Log4j) with ITestListener to customise result output and print the same to log file.
- * https://qavalidation.com/2018/08/combine-testnglistener-with-log4j-for-selenium-result-reporting.html/
+ * https://qavalidation.com/2018/08/combine-testnglistener-with-log4j-for-selenium-result-reporting.html/ 
  */
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
